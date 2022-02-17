a52e89b48547e9930b3d2a1718c4cb97236663a6
mohammad-mobarak-hossen <mdmobarakhossen112@gmail.com>
2022-02-17 10:54:42 +0600

    js

--- money-bank.js
+++ money-bank.js
@@ -2,6 +2,9 @@ document.getElementById('calculate-button').addEventListener('click', function n
     let totalBalanceInput = document.getElementById('total-income')
     let totalBalanceNumber = parseFloat(totalBalanceInput.value);
     let totalBalance = totalBalanceNumber;
+    if (totalBalance > 0) {
+
+    }
 
     // expenses total
     let foodInput = document.getElementById('food-value');
@@ -32,16 +35,36 @@ document.getElementById('calculate-button').addEventListener('click', function n
     restBalanceInput.innerText = totalExpensesMinus
 });
 
+// save Button calculate
+
 document.getElementById('save').addEventListener('click', function name(params) {
 
     let saveInput = document.getElementById('save-input');
     let saveNumber = parseFloat(saveInput.value);
     let saveTotal = saveNumber;
 
-    let total = totalExpensesMinus * saveTotal;
-    let totalinput = total / 100;
-    console.log(totalinput);
 
+
+    let restBalanceInput = document.getElementById('rest-balance')
+    let restBalanceNumber = parseFloat(restBalanceInput.innerText)
+    let totalRestBalance = restBalanceNumber;
+
+    let savingAmount = totalRestBalance * saveTotal / 100;
+
+
+
+    let savingAmountInput = document.getElementById('saving-amount')
+    let savinAmountNumber = parseFloat(savingAmountInput.innerText);
+    let totalSavingAmount = savinAmountNumber;
+    savingAmountInput.innerText = savingAmount;
+
+
+
+    //  remaining balance 
+    let remainingBalanceInput = document.getElementById('remaining-balance');
+    let remainingBalanceNumber = parseFloat(remainingBalanceInput.innerText);
+    let totalRemainingBalance = remainingBalanceNumber;
+    remainingBalanceInput.innerText = totalRestBalance - savingAmountInput.innerText
 
 })
 
